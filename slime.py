f65d144dc84ba7f89357cb020b3d7df9c296e6ba
charlie knox <charlieknox704@gmail.com>
2024-01-28 16:27:40 +0000

    tutorial changes

--- slime.py
+++ slime.py
@@ -3,8 +3,9 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
-test_surface  = pygame.Surface((100, 200))
-test_surface.fill('Red')
+test_surface  = pygame.image.load('game_images/Famous-old-video-game-scene.png.webp')
+test_surface = pygame.transform.scale(test_surface, (800, 400)).convert_alpha()
+
 
 test_font = pygame.font.Font(None,50)
 
@@ -12,17 +13,17 @@ test_font = pygame.font.Font(None,50)
 # for importing an image: pygame.image.load('graphics/sky/image.png')
 # any kind of graphical import will be its own surface 
 
-
 # pygame will overlay images depending on which image was last called so keep that in mind!
 
 test__font_surface = test_font.render('My game', False, 'Green')
 
 pacman_surface = pygame.image.load('game_images/Pacman.svg')
-pacman_surface = pygame.transform.scale(pacman_surface, (20, 20))
-
-pacman_x_position = 0
-pacman_y_position = 0
-
+pacman_surface = pygame.transform.scale(pacman_surface, (15, 15)).convert_alpha()
+pacman_x_position = 170
+pacman_y_position = 80
+# takes a surface and puts a rectangle around it 
+pacman_rec = pacman_surface.get_rect(topleft = (pacman_x_position, pacman_y_position))
+first = 1
 
 
 while True: 
@@ -37,9 +38,17 @@ while True:
 
     screen.blit(test_surface, (0,0))
     screen.blit(test__font_surface, (300, 50))
-    screen.blit(pacman_surface, (pacman_x_position, pacman_y_position))
-    pacman_x_position += 1
-    if pacman_x_position > 700 or pacman_y_position > 400:
-        pacman_x_position = 0
-        pacman_y_position = 0
+    # this way we can associate the pacman surface with the pac man rectangle
+    # to move the image, you move the rectangle rather than the surface itself... 
+    pacman_rec.left += 1
+    screen.blit(pacman_surface, pacman_rec)
+    # printing out rectangle is really useful for dimensions purposes... 
+    mouse_position = pygame.mouse.get_pos()
+    if pacman_rec.collidepoint((mouse_position)): 
+        print('collision' + str(first)) 
+        first += 1 
+
+    if pacman_rec.x >= 620: pacman_rec.x  = 170
     pygame.display.update()
+
+
